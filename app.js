a46e674c737f58267e077f34a7099bf4835d2efe
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-16 17:22:37 -0600

    Commit 1 - Clicker done

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log("homework due mon");
+
+
+// make obj called clicker
+let clicker = {
+    clickCount: 0,
+// click method adds 1 to clickCount and keeps the value updated
+    click() {
+     ++this.clickCount;
+    }
+};
+
