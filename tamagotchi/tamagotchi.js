bad481c567918cb9cebb16377880b165c6a821c2
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-17 14:32:04 -0600

    Commit 21 - the player can knock out a Tamogatchi

--- tamagotchi/tamagotchi.js
+++ tamagotchi/tamagotchi.js
@@ -63,7 +63,12 @@ let player = {
         tamagotchi2.foodInTummy += 1;
     },
     medicateTamagotchi(tamagotchi){
+        // add and assin 1 to the parameter tomagotchi so can call with specified tomagotchi as argument later
         tamagotchi.health += 1;
+    },
+    knockOutTamagotchi(tamagotchi){
+        // add and assign an increase of 1 to restedness of param tamagotchi to call with specific tamagotchi as argument later
+        tamagotchi.restedness += 1;
     }
 };
 player.sayName();
@@ -71,9 +76,16 @@ player.feedTamagotchi();
 // check if foodInTummy updated for both after calling method
 console.log(tamagotchi1.foodInTummy);
 console.log(tamagotchi2.foodInTummy);
+
 // check tamagotchi1 health level before increasing
 console.log(tamagotchi1.health);
 // call method to increase health
 player.medicateTamagotchi(tamagotchi1);
 // check to see if health increased by 1
 console.log(tamagotchi1.health);
+
+// check restedness of tamagotchi1 before
+console.log(tamagotchi1.restedness);
+player.knockOutTamagotchi(tamagotchi1);
+// check restedness of tamagotchi1 after
+console.log(tamagotchi1.restedness);
