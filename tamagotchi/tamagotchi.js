b609a116eeaa239f3c6ec50921d9201293c2e0da
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-17 14:02:19 -0600

    Commit 16 - made a 2nd Tamogatchi

--- tamagotchi/tamagotchi.js
+++ tamagotchi/tamagotchi.js
@@ -21,3 +21,25 @@ let tamagotchi1 = {
 tamagotchi1.cry();
 tamagotchi1.puke();
 tamagotchi1.yawn();
+
+let tamagotchi2 = {
+    name: "Rainbow",
+    creatureType: "Lizard",
+    foodInTummy: 15,
+    restedness: 15,
+    health: 15,
+    cry(){
+        // decrease foodInTummy by one and store new value with -= operator
+        this.foodInTummy -= 1;
+        console.log("Waaaaa!! My tummy is now " + this.foodInTummy + " full.")
+    },
+    puke(){
+        console.log(this.name + " Bleeeh :0~~")
+    },
+    yawn(){
+        console.log(this.name + " Yaaaawwwn")
+    }
+}
+tamagotchi2.cry();
+tamagotchi2.puke();
+tamagotchi2.yawn();
