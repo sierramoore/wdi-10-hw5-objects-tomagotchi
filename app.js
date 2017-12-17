6a6285209a5a1b71e8194da8f34df6a7eb0da8f3
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-16 18:18:35 -0600

    Commit 3 - Greeter done

--- app.js
+++ app.js
@@ -1,29 +1,45 @@
 console.log("homework due mon");
 
 //*************CLICKER******************************
-// make obj called clicker
-let clicker = {
-    clickCount: 0,
-// click method adds 1 to clickCount and keeps the value updated
-    click() {
-     ++this.clickCount;
+// // make obj called clicker
+// let clicker = {
+//     clickCount: 0,
+// // click method adds 1 to clickCount and keeps the value updated
+//     click() {
+//      ++this.clickCount;
+//     }
+// };
+// //*****************ITS ALL ABOUT ME*************************
+// let myInformation = {
+//     name: "Sierra",
+//     age: 23,
+//     fact: "I love cats",
+//     statement(){
+//         return("My name is " + this.name + " and I am " + this.age + " years old and " + this.fact)
+//     },
+//     older(){
+//         ++this.age;
+//     },
+//     nameChange(){
+//         this.name = "Rumpelstiltskin"
+//     }
+// };
+// myInformation.older();
+// myInformation.nameChange();
+// console.log(myInformation.name);
+
+//*****************GREETER*************************
+let greeter = {
+    hello(name){
+        return("Why hello there, " + name)
+    },
+    goodbye(name){
+        return("Benos noche, " + name)
+    },
+    whoAreYou(name){
+        return("Oh right! " + name + " how could I forget...!")
     }
 };
-//*****************ITS ALL ABOUT ME*************************
-let myInformation = {
-    name: "Sierra",
-    age: 23,
-    fact: "I love cats",
-    statement(){
-        return("My name is " + this.name + " and I am " + this.age + " years old and " + this.fact)
-    },
-    older(){
-        ++this.age;
-    },
-    nameChange(){
-        this.name = "Rumpelstiltskin"
-    }
-};
-myInformation.older();
-myInformation.nameChange();
-console.log(myInformation.name);
+console.log(greeter.hello("Luka"));
+console.log(greeter.goodbye("Luka"));
+console.log(greeter.whoAreYou("Luka"));
