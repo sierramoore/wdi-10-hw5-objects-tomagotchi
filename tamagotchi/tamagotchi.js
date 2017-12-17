9e95d81ea46570f5d53264bd58e70b9f7f16e17a
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-17 17:41:10 -0600

    Commit 22 - brought to life Tamogatchi (globally)

--- tamagotchi/tamagotchi.js
+++ tamagotchi/tamagotchi.js
@@ -9,7 +9,7 @@ let tamagotchi1 = {
     cry(){
         // decrease foodInTummy by one and store new value with -= operator
         this.foodInTummy -= 1;
-        console.log("Waaaaa!! My tummy is now " + this.foodInTummy + " full.")
+        console.log(this.name + " Waaaaa!! My tummy is now " + this.foodInTummy + " full.")
     },
     puke(){
         //decrease health by one and store new value with -= operator
@@ -22,9 +22,9 @@ let tamagotchi1 = {
         console.log(this.name + " Yaaaawwwn. My resetness is now " + this.restedness)
     }
 };
-tamagotchi1.cry();
-tamagotchi1.puke();
-tamagotchi1.yawn();
+// tamagotchi1.cry();
+// tamagotchi1.puke();
+// tamagotchi1.yawn();
 
 let tamagotchi2 = {
     name: "Rainbow",
@@ -35,7 +35,7 @@ let tamagotchi2 = {
     cry(){
         // decrease foodInTummy by one and store new value with -= operator
         this.foodInTummy -= 1;
-        console.log("Waaaaa!! My tummy is now " + this.foodInTummy + " full.")
+        console.log(this.name + " Waaaaa!! My tummy is now " + this.foodInTummy + " full.")
     },
     puke(){
         //decrease health by one and store new value with -= operator
@@ -46,11 +46,11 @@ let tamagotchi2 = {
         //decrease restedness by one and store new value with -= operator
         this.restedness -=1;
         console.log(this.name + " Yaaaawwwn. My resetness is now " + this.restedness)
-    }
+    },
 };
-tamagotchi2.cry();
-tamagotchi2.puke();
-tamagotchi2.yawn();
+// tamagotchi2.cry();
+// tamagotchi2.puke();
+// tamagotchi2.yawn();
 
 let player = {
     name: "Jihiro",
@@ -71,21 +71,89 @@ let player = {
         tamagotchi.restedness += 1;
     }
 };
-player.sayName();
-player.feedTamagotchi();
-// check if foodInTummy updated for both after calling method
-console.log(tamagotchi1.foodInTummy);
-console.log(tamagotchi2.foodInTummy);
+// player.sayName();
+// player.feedTamagotchi();
+// // check if foodInTummy updated for both after calling method
+// console.log(tamagotchi1.foodInTummy);
+// console.log(tamagotchi2.foodInTummy);
+//
+// // check tamagotchi1 health level before increasing
+// console.log(tamagotchi1.health);
+// // call method to increase health
+// player.medicateTamagotchi(tamagotchi1);
+// // check to see if health increased by 1
+// console.log(tamagotchi1.health);
+//
+// // check restedness of tamagotchi1 before
+// console.log(tamagotchi1.restedness);
+// player.knockOutTamagotchi(tamagotchi1);
+// // check restedness of tamagotchi1 after
+// console.log(tamagotchi1.restedness);
 
-// check tamagotchi1 health level before increasing
-console.log(tamagotchi1.health);
-// call method to increase health
-player.medicateTamagotchi(tamagotchi1);
-// check to see if health increased by 1
-console.log(tamagotchi1.health);
+//globally set timer for tamagotchis to decrease their health, foodInTummy, and restfullness
+// added name to console log when both tamagatchis cry to test
 
-// check restedness of tamagotchi1 before
-console.log(tamagotchi1.restedness);
-player.knockOutTamagotchi(tamagotchi1);
-// check restedness of tamagotchi1 after
-console.log(tamagotchi1.restedness);
+const setInterval = {
+
+    // tamgochi.cry needs to be inside another function bkus tamagotchi.cry is a method
+    // and serInterval expects a regular function when u give it a method it will still call it as a regular function and will loose the object info.
+    // function after setInterval is like a proxy frunction will call tomagotchi cry
+    //same as -----
+    // hungerTimer(tamagotchi){
+    //     let proxyFunction = function(){
+    //         tamagotchi.cry()
+    //     }
+    //     window.setInterval(proxyFunction, 6000)
+    // } --------
+    // make tamagotchi cry every 6 sec
+    hungerTimer(tamagotchi){
+        window.setInterval(function(){tamagotchi.cry()}, 6000)
+    },
+    // make tamagotchi yawn every 10 sec
+    yawnTimer(tamagotchi){
+        window.setInterval(function(){tamagotchi.yawn()}, 10000)
+    },
+    // make tamagotchi puke every 25 sec
+    sickTimer(tamagotchi){
+        window.setInterval(function(){tamagotchi.puke()}, 25000)
+    }
+};
+setInterval.hungerTimer(tamagotchi1);
+setInterval.yawnTimer(tamagotchi1);
+setInterval.sickTimer(tamagotchi1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
