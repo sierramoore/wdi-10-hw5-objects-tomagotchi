88c9c1a63d3ec282c6473e316137663ce351d8ad
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-17 13:59:43 -0600

    Commit 15 - updated puke and yawn methods

--- tamagotchi/tamagotchi.js
+++ tamagotchi/tamagotchi.js
@@ -1,19 +1,21 @@
 console.log("--tamagachi--");
 
 let tamagotchi1 = {
-    name: "starry",
+    name: "Starry",
     creatureType: "Dragon",
     foodInTummy: 10,
     restedness: 10,
     health: 10,
     cry(){
-        console.log("Waaaaa!!")
+        // decrease foodInTummy by one and store new value with -= operator
+        this.foodInTummy -= 1;
+        console.log("Waaaaa!! My tummy is now " + this.foodInTummy + " full.")
     },
     puke(){
-        console.log("Bleeeh :0~~")
+        console.log(this.name + " Bleeeh :0~~")
     },
     yawn(){
-        console.log("Yaaaawwwn")
+        console.log(this.name + " Yaaaawwwn")
     }
 }
 tamagotchi1.cry();
