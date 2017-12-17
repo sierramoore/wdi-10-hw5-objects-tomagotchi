44d44dcb38caad6590ef946d42f3c284090abd1e
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-17 13:49:56 -0600

    Commit 12 - Made a Tamagotchi object

---
+++ tamagotchi/tamagotchi.js
@@ -0,0 +1,13 @@
+console.log("--tamagachi--");
+
+let tamagotchi1 = {
+    name: "starry",
+    creatureType: "Dragon",
+    foodInTummy: 10,
+    restedness: 10,
+    health: 10,
+    cry(){
+        console.log("Waaaaa!!")
+    }
+}
+tamagotchi1.cry();
